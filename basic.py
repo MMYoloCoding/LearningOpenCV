c6e63c9d074f1309edd9f3c0cdcd11457448d6ff
MMYoloCoding <michael223479@gmail.com>
2023-06-26 10:12:39 +0800

    -a

--- basic.py
+++ basic.py
@@ -2,9 +2,33 @@ import cv2 as cv
 
 img = cv.imread('Photos/cat.jpg')
 
-cv.imshow('Cat', img)
+cv.imshow('lady', img)
 
-# Convering to grayscale
+# Convering to grayscale (intensity distribution of pixels) 
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# cv.imshow('Cat', gray)
 
+# Blur 
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+# cv.imshow('blur', blur)
 
+# Edge Cascade
+canny = cv.Canny(blur, 125, 175)
+# cv.imshow("Canny", canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=3)
+# cv.imshow('Dilated', dilated)
+
+#Eroding
+eroded = cv.erode(dilated, (7,7) , iterations=3)
+# cv.imshow('Eroded', eroded)
+
+# Resize (INTER_AREA for shrinking, INTER_LINE for enlarging)
+resized = cv.resize(img, (5000,3000), interpolation=cv.INTER_CUBIC)
+# cv.imshow('resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('cropped', cropped)
 cv.waitKey(0)
--- read.py
+++ read.py
@@ -13,7 +13,11 @@ capture = cv.VideoCapture('Videos/dog.mp4')
 
 while True:
     isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
+    # frame_text = cv.putText(frame, "Hello I am a cute puppy", (255,255),cv.FONT_HERSHEY_TRIPLEX, 1.0 ,(0,0,255), 2)
+    # frame = cv.Canny(frame, 125,175)
+    # filtered_frame = cv.blur(frame, (111,111))
+    cropped = frame[0:5000, 500:5000]
+    cv.imshow('Video', cropped)
 
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
